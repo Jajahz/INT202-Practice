76c5b976dc8126593e15526112bcac619c84b6c1
Asamaporn Pladsri <asamaporn.jajah@mail.kmutt.ac.th>
2021-10-11 10:31:17 +0700

    Week9

--- ClassicModelWeb/src/main/java/sit/int202/classicmodelweb/Main.java
+++ ClassicModelWeb/src/main/java/sit/int202/classicmodelweb/Main.java
@@ -3,12 +3,19 @@ package sit.int202.classicmodelweb;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Persistence;
+import sit.int202.classicmodelweb.entities.Office;
 
-    public class Main {
+public class Main {
         public static void main(String[] args) {
             EntityManagerFactory emf =  Persistence.createEntityManagerFactory("default");
             EntityManager em = emf.createEntityManager();
-            Office office = em.find(Office.class, "1");
-            System.out.println(office);
+            Office office = em.find(Office.class, "2");
+            System.out.println(office.getId() + " : " + office.getCity());
+            System.out.println("----------------------------------------");
+            office.getEmployeeList().forEach(e -> {
+                System.out.printf("%5d %-9s %-15s %s\n",
+                        e.getId(), e.getFirstName(), e.getLastName(), e.getEmail());
+            });
+            em.close();
         }
     }
